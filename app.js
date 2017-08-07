522520db26fa258c8dcc47adef3b4d9754427ee9
Shrenik <shrenikshah94@gmail.com>
2017-08-07 04:27:37 +0000

    updated login page

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes          = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp_v12");
+// mongoose.connect("mongodb://localhost/yelp_camp_v12");
+mongoose.connect("mongodb://shrenik:shrenik@ds161041.mlab.com:61041/ibackpackamerica");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
